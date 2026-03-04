d85ff34cc1f103f22f931297328f2a8b1463217f
Igor Tatarnikov <61896994+IgorTatarnikov@users.noreply.github.com>
2026-03-04 12:32:24 +0000

    Fix link to allen_mouse terminology (#447)

--- docs/source/aws_examples_source/explore_atlas.py
+++ docs/source/aws_examples_source/explore_atlas.py
@@ -43,7 +43,7 @@ from matplotlib import colormaps as cm
 
 s3_bucket_stub = "s3://brainglobe/atlas/{}"
 annotation_uri = s3_bucket_stub.format("annotation-sets/allen-adult-mouse-annotation/2017/annotation.ome.zarr")
-terminologies_uri = s3_bucket_stub.format("terminologies/allen-adult-mouse-terminology/2017/terminology.csv")
+terminologies_uri = s3_bucket_stub.format("terminologies/allen_mouse-terminology/1_2/terminology.csv")
 
 terminologies_df = pd.read_csv(terminologies_uri, storage_options={"anon": True})
 
