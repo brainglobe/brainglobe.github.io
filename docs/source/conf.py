133be10b45949b4ac756420ae003941f481c1666
Igor Tatarnikov <igor.tatarnikov@gmail.com>
2026-04-24 16:00:09 +0100

    Revert new line removal and rely on default linkcheck state

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -94,6 +94,7 @@ exclude_patterns = [
     "**_examples_source",
 ]
 
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
