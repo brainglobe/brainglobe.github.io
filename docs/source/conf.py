ff3e40f2e6435b223e3d121f3a3268da4aeff358
Ardavan sh <ardavan@Ardavans-MacBook-Pro.local>
2026-07-07 15:04:38 -0400

    Add robots.txt for sitemap discovery

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -96,6 +96,7 @@ exclude_patterns = [
 
 # -- Options for HTML output -------------------------------------------------
 
+html_extra_path = ["robots.txt"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
