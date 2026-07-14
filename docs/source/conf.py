acd683c40e8cb74136063f4a4dc12438d1e8833f
Igor Tatarnikov <igor.tatarnikov@gmail.com>
2026-07-14 15:50:53 +0100

    Unify the html_extra_path variables

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -96,9 +96,6 @@ exclude_patterns = [
 
 # -- Options for HTML output -------------------------------------------------
 
-# Include robots.txt in the built site.
-html_extra_path = ["robots.txt"]
-
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
@@ -115,7 +112,7 @@ sitemap_url_scheme = "{link}"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_extra_path = ["_redirects"]
+html_extra_path = ["_redirects", "robots.txt"]
 
 html_css_files = [
     ("css/custom.css", {"priority": 100}),
